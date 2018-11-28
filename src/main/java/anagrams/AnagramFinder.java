2127b4ebb7905565da241d1615178befb938f232
purushothaman.ramraj <purushothaman.ramraj@glassdoor.com>
2018-11-28 12:11:41 -0800

    pass in function name instead of lambda

--- src/main/java/anagrams/AnagramFinder.java
+++ src/main/java/anagrams/AnagramFinder.java
@@ -7,18 +7,18 @@ import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
 
 public class AnagramFinder {
+    private static String sortCharsInString(String s) {
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+
     public List<List<String>> findAnagrams(List<String> wordSet) {
         return wordSet.stream()
-                .collect(groupingBy(s -> sortCharsInString(s)))
+                .collect(groupingBy(AnagramFinder::sortCharsInString))
                 .values()
                 .stream()
                 .filter(list -> list.size() > 1)
                 .collect(toList());
-    }
-
-    private String sortCharsInString(String s) {
-        char[] chars = s.toCharArray();
-        Arrays.sort(chars);
-        return new String(chars);
     }
 }
