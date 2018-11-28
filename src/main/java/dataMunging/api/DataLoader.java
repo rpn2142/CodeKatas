3826821d386f3d4ceb4ccf37a2e328fafc9a4bbd
purushothaman.ramraj <purushothaman.ramraj@glassdoor.com>
2018-11-28 12:30:14 -0800

    use streams and file utils

--- src/main/java/dataMunging/api/DataLoader.java
+++ src/main/java/dataMunging/api/DataLoader.java
@@ -1,8 +1,13 @@
 package dataMunging.api;
 
-import java.io.*;
-import java.util.ArrayList;
+import com.google.common.collect.Lists;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * Created by pramraj on 3/4/18.
@@ -10,46 +15,16 @@ import java.util.List;
 public abstract class DataLoader {
 
     public List<Data> load(File file) {
-        List<Data> dataList = new ArrayList<>();
-        loadFile(file, dataList);
-
-        return dataList;
-
-    }
-
-    private void loadFile(File file, List<Data> dataList) {
-        BufferedReader br = null;
-
         try {
-            FileInputStream fis = new FileInputStream(file);
-            br = new BufferedReader(new InputStreamReader(fis));
-
-            String line = null;
-            while ((line = br.readLine()) != null)
-                addToList(dataList, line);
-
-        } catch(Exception e) {
+            return FileUtils.readLines(file, "utf-8")
+                    .stream()
+                    .map(line -> toData(line.trim()))
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            close(br);
         }
-    }
-
-    private void addToList(List<Data> dataList, String line) {
-        line = line.trim();
-        Data data = toData(line);
-        if( data != null )
-            dataList.add(data);
-    }
-
-    private void close(BufferedReader br) {
-        if( br != null ) {
-            try {
-                br.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        return Lists.newArrayList();
     }
 
     protected abstract Data toData(String line);
